befb2c93a087cd5167a070f09c1465e915a1c842
danila nikitin <danila.n2015@yandex.ru>
2023-09-26 20:11:27 +0300

    add mongo settings and replace properties to yml

--- src/main/java/com/jfund/currencypairsservice/CurrencyPairsServiceApplication.java
+++ src/main/java/com/jfund/currencypairsservice/CurrencyPairsServiceApplication.java
@@ -3,6 +3,7 @@ package com.jfund.currencypairsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +12,4 @@ public class CurrencyPairsServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyPairsServiceApplication.class, args);
 	}
-
 }
