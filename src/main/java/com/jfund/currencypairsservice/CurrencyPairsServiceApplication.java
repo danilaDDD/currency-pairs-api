a8bceb6cca67b3cf619973799e4e55d225a1f0fc
danila nikitin <danila.n2015@yandex.ru>
2023-09-26 17:49:52 +0300

    init repository

---
+++ src/main/java/com/jfund/currencypairsservice/CurrencyPairsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.jfund.currencypairsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CurrencyPairsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyPairsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfund/currencypairsservice/CurrencyPairsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfund.currencypairsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyPairsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
