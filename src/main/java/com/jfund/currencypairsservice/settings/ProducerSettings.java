79f627c2e9993c36cf9d0150248e512f77733ecd
danila nikitin <danila.n2015@yandex.ru>
2025-01-16 19:31:23 +0300

    remove unused imports in ProducerSettings

--- src/main/java/com/jfund/currencypairsservice/settings/ProducerSettings.java
+++ src/main/java/com/jfund/currencypairsservice/settings/ProducerSettings.java
@@ -1,8 +1,6 @@
 package com.jfund.currencypairsservice.settings;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
